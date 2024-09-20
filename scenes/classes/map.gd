9d95b7f167c1c77e37dc8c3242c6f7665bda9444
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-20 13:23:51 -0400

    Warn if map doesn't have a SpawnLocation

--- scenes/classes/map.gd
+++ scenes/classes/map.gd
@@ -1,11 +1,21 @@
+@tool
 class_name Map extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+	
+	if !has_node("SpawnLocation"):
+		warnings.append("Map must have a SpawnLocation.")
+	
+	return warnings
