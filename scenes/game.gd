6646d2c101b9bbb542cf51400c3c7b9d08bcb7ee
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-20 12:44:12 -0400

    Add starting positions to maps

--- scenes/game.gd
+++ scenes/game.gd
@@ -14,19 +14,32 @@ func _process(delta: float) -> void:
 	pass
 
 
-# Instantiates and adds players and loads the map
+# Adds players and loads the map
 func new_game(karts: Array[Node], map: Map) -> void:
+	# Add players
 	for e in karts:
 		add_child(e)
 		players.append(e)
+	
+	# Load map
 	load_map(map)
-	print(map.get_path())
 
 
 # Loads a new map and moves the players to their starting positions
 func load_map(map: Map) -> void:
+	# Instantiate the map
 	var map_node = map.instantiate()
+	
+	# Remove the last map if one was loaded
 	if loaded_map:
 		loaded_map.get_parent().remove_child(loaded_map)
+	
+	# Add the map to the scene
 	add_child(map_node)
+	
+	# Save the map so it can be unloaded later
 	loaded_map = map_node
+	
+	# Move the players to the starting position
+	for player in players:
+		player.transform = loaded_map.get_node("SpawnLocation").transform
