9598d81f9fe2d184be95952d76de665dddb58d53
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-23 12:08:25 -0400

    Unload karts when character selection buttons are hidden Should reduce memory usage while in game.

--- scenes/classes/character_preview/character_button.gd
+++ scenes/classes/character_preview/character_button.gd
@@ -1,25 +1,42 @@
 class_name CharacterButton extends TextureButton
 
+var character_preview_world: Node
+
 @export var character_preview_world_scene: PackedScene = preload("res://scenes/classes/character_preview/character_preview_world.tscn")
 @export var kart: KartMetadata:
 	set(new_kart):
 		# Set the kart to the new kart
 		kart = new_kart
-		
-		# Create a new CharacterPreviewWorld for the kart
-		var character_preview_world = character_preview_world_scene.instantiate()
-		character_preview_world.kart = new_kart
-		add_child(character_preview_world)
-		
-		# Set the button texture to the viewport texture
-		texture_normal = character_preview_world.get_texture()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	self.hidden.connect(_on_hide)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_hide() -> void:
+	# Remove the texture so Godot doesn't try to draw it and error because the viewport doesn't exist
+	if texture_normal:
+		texture_normal = null
+	
+	# Unload the character preview world to save memory
+	if character_preview_world:
+		character_preview_world.queue_free()
+
+
+func _draw() -> void:
+	# Create a new CharacterPreviewWorld for the kart if it doesn't already exist
+	if !character_preview_world:
+		character_preview_world = character_preview_world_scene.instantiate()
+		add_child(character_preview_world)
+	
+	# Set the preview world kart
+	character_preview_world.kart = kart
+	
+	# Set the button texture to the viewport texture
+	texture_normal = character_preview_world.get_texture()
--- scenes/classes/character_preview/character_preview_world.gd
+++ scenes/classes/character_preview/character_preview_world.gd
@@ -1,14 +1,22 @@
 extends SubViewport
 
+var kart_node: Kart
+
 @export var kart: KartMetadata:
 	set(new_kart):
+		# Swap the kart out if the kart has changed
+		if kart != new_kart:
+			# If a kart already exists, delete it
+			if kart_node:
+				kart_node.queue_free()
+			
+			# Instantiate the new kart
+			kart_node = new_kart.instantiate()
+			kart_node.kart_preview = true
+			add_child(kart_node)
+		
 		# Set the kart to the new kart
 		kart = new_kart
-		
-		# Instantiate the kart
-		var kart_node: Kart = kart.instantiate()
-		kart_node.kart_preview = true
-		add_child(kart_node)
 
 
 # Called when the node enters the scene tree for the first time.
