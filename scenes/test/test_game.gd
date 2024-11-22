e1cc836bf8f76303f83373118573d9101ce92081
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-11-22 12:17:26 -0500

    Fix the TestGame

--- scenes/test/test_game.gd
+++ scenes/test/test_game.gd
@@ -1,9 +1,16 @@
 extends Node3D
 
+@export var kart_list: KartList
 @export var karts: Array[KartMetadata]
 @export var map: MapMetadata
+
+@onready var game: Game = $Game
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Game.new_game(karts, map)
+	game.add_player(1)
+	for kart in karts:
+		game.set_player_kart.rpc_id(1, kart_list.get_kart_id(kart))
+	game.load_map(map)
+	game.start_game()
