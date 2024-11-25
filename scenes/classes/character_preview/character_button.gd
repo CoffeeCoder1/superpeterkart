58f5c1288ebccbed62a3a5a20a875b3cda5921a9
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-11-25 12:12:34 -0500

    Fix the rendering of the CharacterButton

--- scenes/classes/character_preview/character_button.gd
+++ scenes/classes/character_preview/character_button.gd
@@ -1,7 +1,12 @@
 class_name CharacterButton extends Button
 
 var character_preview_world: CharacterPreviewWorld
-var kart_node: Kart
+var kart_node: Kart:
+	set(new_kart_node):
+		if is_instance_valid(character_preview_world):
+			character_preview_world.kart = new_kart_node
+		
+		kart_node = new_kart_node
 
 @export var character_preview_world_scene: PackedScene = preload("res://scenes/classes/character_preview/character_preview_world.tscn")
 @export var kart: KartMetadata:
@@ -16,19 +21,18 @@ var kart_node: Kart
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.hidden.connect(_on_hide)
+	hidden.connect(_on_hide)
+	draw.connect(_on_draw)
+	expand_icon = true
 
 
 func _load_kart(kart: KartMetadata) -> void:
-	if is_instance_valid(character_preview_world):
-		# If a kart already exists, delete it
-		if is_instance_valid(kart_node):
-			kart_node.queue_free()
-		
-		# Instantiate the new kart
-		kart_node = kart.instantiate()
-		
-		character_preview_world.kart = kart_node
+	# If a kart already exists, delete it
+	if is_instance_valid(kart_node):
+		kart_node.queue_free()
+	
+	# Instantiate the new kart
+	kart_node = kart.instantiate()
 
 
 func _on_hide() -> void:
@@ -41,14 +45,19 @@ func _on_hide() -> void:
 		character_preview_world.queue_free()
 
 
-func _draw() -> void:
+func _on_draw() -> void:
 	# Create a new CharacterPreviewWorld for the kart if it doesn't already exist
-	if !character_preview_world:
+	if !is_instance_valid(character_preview_world):
 		character_preview_world = character_preview_world_scene.instantiate()
 		add_child(character_preview_world)
 	
+	# Load the kart if it doesn't exist
+	if !is_instance_valid(kart_node):
+		_load_kart(kart)
+	
 	# Set the preview world kart
-	_load_kart(kart)
+	character_preview_world.kart = kart_node
 	
 	# Set the button texture to the viewport texture
+	await RenderingServer.frame_post_draw
 	icon = character_preview_world.get_texture()
