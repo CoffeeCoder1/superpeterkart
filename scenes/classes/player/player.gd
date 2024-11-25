3941d793d14da0b8281e0e12faa59c8ae8d8c6d3
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-11-25 11:37:40 -0500

    Add a placeholder character nickname

--- scenes/classes/player/player.gd
+++ scenes/classes/player/player.gd
@@ -1,7 +1,7 @@
 class_name Player extends Node
 
 ## Nickname specified by the player.
-@export var nick: String
+@export var nick: String = "There's no way to set this yet so you'll just be named this."
 ## Kart that the player is going to use.
 @export var kart_metadata: KartMetadata
 ## Kart that is loaded for the player.
