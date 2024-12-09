179dae77b2b9c43bc0cd1e441ab96af29efae4bd
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-12-09 12:01:49 -0500

    Add a lap counter to the HUD

--- scenes/ui/heads_up_display/heads_up_display.gd
+++ scenes/ui/heads_up_display/heads_up_display.gd
@@ -3,9 +3,11 @@ class_name HeadsUpDisplay extends Control
 @export var player: Player
 
 @onready var speed_label: Label = $SpeedLabel
+@onready var lap_label: Label = $LapLabel
 
 
 func _process(delta: float) -> void:
-	if player:
+	if is_instance_valid(player):
 		if is_instance_valid(player.kart):
 			speed_label.text = str(player.kart.get_speed())
+		lap_label.text = str(player.lap)
