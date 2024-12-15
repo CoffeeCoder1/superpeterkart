14ee4311e158065b55471bb52ae4b0f4f938919e
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-12-15 17:53:31 -0500

    Remove WorldEnvironment when map is hidden

--- scenes/classes/map/map.gd
+++ scenes/classes/map/map.gd
@@ -1,8 +1,13 @@
 @tool
 class_name Map extends Node3D
 
+@export var environment: Environment
+@export var compositor: Compositor
+
 @onready var spawn_location: Marker3D = $SpawnLocation
 @onready var drive_path: Path3D = $DrivePath
+
+var world_environment: WorldEnvironment
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -18,6 +23,25 @@ func _get_configuration_warnings() -> PackedStringArray:
 		warnings.append("Map must have a DrivePath.")
 	
 	return warnings
+
+
+func _ready() -> void:
+	visibility_changed.connect(_on_visibility_changed)
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		# Create a world environment if needed
+		if is_instance_valid(environment) or is_instance_valid(compositor):
+			if not is_instance_valid(world_environment):
+				world_environment = WorldEnvironment.new()
+				add_child(world_environment)
+			world_environment.environment = environment
+			world_environment.compositor = compositor
+	else:
+		# Free the world environment if it exists
+		if is_instance_valid(world_environment):
+			world_environment.queue_free()
 
 
 func get_spawn_location() -> Transform3D:
